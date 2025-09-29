ea7dea8dfc23d4769bbce06412330a1140c9df88
Nitinshukla108 <132742675+Nitinshukla108@users.noreply.github.com>
2025-09-29 08:41:45 +0000

    02_datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,39 @@
+
+"use strict";  // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3)   // code readibility should be high
+
+console.log("nitin");
+
+
+
+let name = "nitin";
+let age = 19;
+let isLoggedIn = false;
+let state;
+
+// premitive data type
+// number
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefinder => value not defined
+// symbol => unique
+
+
+
+// object
+
+
+console.log(typeof "nitin"); // string
+console.log(typeof age); // number
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+

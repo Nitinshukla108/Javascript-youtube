4785965bba1280db1caeb98cc242db191a80041f
Nitinshukla108 <132742675+Nitinshukla108@users.noreply.github.com>
2025-09-29 09:20:57 +0000

    updated 03_conversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -36,3 +36,49 @@ let stringNumber = String(someNumber);
 console.log(typeof stringNumber);
 console.log(stringNumber);
 
+
+
+// **************Operation**************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2***2);
+console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " nitin"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2); //12
+console.log(1 + "2");  //12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+
+console.log(true);  // true
+console.log(+true); // 1
+console.log(true+);  // error
+console.log(+"");  // 0
+
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+

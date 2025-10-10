47abb390618be4b4ec3ba7184ddea86b31ed6750
Nitinshukla108 <132742675+Nitinshukla108@users.noreply.github.com>
2025-10-10 13:59:33 +0000

    updated 01_function

--- 03_basics_Lv3/01_function.js
+++ 03_basics_Lv3/01_function.js
@@ -53,3 +53,46 @@ console.log(logged());
 
 // and sab kuch avoid krne ke liye hum chaye toh default value phle se hi pass kr skte hai parameter me hi 
 // like userName = "sam" toh user value nahi pass krega toh default value uski sam ho jayega and aagar value pass krega toh over-write ho jayegi value.
+
+
+
+
+// ab dekhte hai aagar user value ek se jada pass kre toh usse kaise handle kre hum
+
+// for that hum use krte rest(...) or ha hum isse spread bhi bolte hai pr dono ko aalg aalg use krte hai
+function shoppingCart(...device){
+    return device
+}
+console.log(shoppingCart(1,2,3,4));
+
+
+
+// ab dekhte hai aagar object ko function me kaise pass kia jata hai aur kaise use kia jata hai 
+
+const user = {
+    userID:1,
+    userName:"nitin"
+}
+function handleObject(anyObject){
+    return `${anyObject.userID} is my ID and my user name is ${anyObject.userName}`
+}
+// console.log(handleObject(user))
+console.log({
+    userID:2,
+    userName:"shukla"
+});
+                     
+// iska ye mtlb hai ki hum object badme bhi create kr skte haii
+
+
+
+// ab same dekhte hai array ke liye 
+
+const myArray = [100,200,300,400]
+function findNumberTwo(numberTwo){
+    return `${numberTwo[1]}`
+}
+console.log(findNumberTwo(myArray))
+console.log(findNumberTwo([200,600,400,500]))
+
+// by that we can see ki hum array bhi badme create kr skte hai jaruri nahi hai ki phle hi banaye

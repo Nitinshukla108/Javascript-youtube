1c96b2a475e179fd457e7ced939dd01c0d9d1a9a
Nitinshukla108 <132742675+Nitinshukla108@users.noreply.github.com>
2025-10-11 14:46:36 +0000

    scope-updated

--- 03_basics_Lv3/02_scopes.js
+++ 03_basics_Lv3/02_scopes.js
@@ -26,3 +26,62 @@ console.log("OUTER:d", d);
 
 
 // so by that you can see the inner d is the block scope and the outer d is the global scope 
+
+
+
+
+// nested scope
+
+function one(){
+    const username = "nitin"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    //console.log(website);
+    two()
+}
+one()
+
+// by this example you can see parent function, child function ki value access nahi kr pa raha hai pr child fucntion parent function ki value access kr raha haii
+
+
+
+if(true){
+    const userName = "nit"
+    if(userName === "nit"){
+        const webSite = " youtube"
+        console.log(userName + webSite);
+    }
+    //console.log(webSite);   
+}
+//console.log(userName);
+
+// same thing happend here too
+
+
+
+
+// +++++++++ interesting ++++++++++
+
+
+console.log(addOne(5));
+
+function addOne(num){
+    return num + 2
+}
+
+
+console.log(addTwo(5));
+
+const addTwo = function(num){
+    return num + 2
+}
+
+
+// to dekha apne dono me difference ek me error throw hua aur ek me nahii 
+// to ye differences hai aagar hum aalg tarike se function declare kare toh
+
+// and isko bolte hai hoisting ki funciton kaha rkha jata haii etc etc ....
